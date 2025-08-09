1f1129b5970ef6e6dbe67db41c6f8b1aaaf111cf
LucianSchipor <schiporlucian20021@gmail.com>
2025-08-09 17:44:20 +0300

    server side

---
+++ server-side/server/Commands/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkAPI;
+using GTANetworkMethods;
+
+namespace server.Commands
+{
+    public class Commands : Script
+    {
+
+        [Command("veh", Alias = "veh")]
+    } 
+}
---
+++ server-side/server/Main.cs
@@ -0,0 +1,13 @@
+﻿using GTANetworkAPI;
+namespace server
+{
+    public class Main : Script
+    {
+
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart()
+        {
+            Console.WriteLine("Ariseu just started!");
+        }
+    }
+}
